1ef68d4ab51ca05e703e9f013a06376acd8dd454
F0rL <kumaloveslife@gmail.com>
2020-09-13 23:42:44 +0800

    基础

--- jike1-24/src/index.ts
+++ jike1-24/src/index.ts
@@ -1,2 +1,11 @@
-let hello : string = 'Hello typescript'
-document.querySelectorAll('.app')[0].innerHTML= hello
+// 原始类型
+let bool: boolean = false
+
+let add = (x: number, y: number) => x + y
+
+let compute: (x: number, y: number) => number
+compute = (a, b) => a + b
+
+let num: number  | undefined | null= 3
+
+num = null
