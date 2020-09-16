25767820395cd7dd1b12bcc269518b37631667de
F0rL <kumaloveslife@gmail.com>
2020-09-16 14:38:33 +0800

    基础应用

--- ts-react-app/src/index.tsx
+++ ts-react-app/src/index.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Hello from './components/demo/Hello';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Hello from './components/demo/Hello'
+import HelloClass from './components/demo/HelloClass'
+import HelloHoc from './components/demo/HelloHoc'
+import HelloHooks from './components/demo/HelloHooks'
 
 ReactDOM.render(
-    <Hello />,
-    document.querySelectorAll('.app')[0]
-);
+  //<HelloClass name="kumaClass"/>,
+  //<HelloHoc name="kumaClass" loading={false} />,
+  <HelloHooks name="kumaxx"/>,
+  document.querySelectorAll('.app')[0]
+)
